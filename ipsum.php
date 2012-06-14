2c198ec52963c48a8a4c759a1ea08149e175ce1e
Adam Patterson <hello@adampatterson.ca>
2012-06-14 10:40:14 -0600

    Reverted IPSUM to the API Call.
    removed Chyrp filters.

--- ipsum.php
+++ ipsum.php
@@ -2,14 +2,11 @@
 class Ipsum extends Modules {
 
 	public function __init() {
-		$this->addAlias("markup_text", "ipsum");
-		$this->addAlias("markup_post_text","ipsum");
 		$this->addAlias("preview", "ipsum", 1);
-		$this->addAlias("preview_post", "ipsum");
 	}
 	
 	public function ipsum($text='') {
-		return str_replace('[ipsum]', "module test", $text);
-		//return str_replace('[ipsum]', file_get_contents('http://loripsum.net/api'), $text);
+		//return str_replace('[ipsum]', "module test", $text);
+		return str_replace('[ipsum]', file_get_contents('http://loripsum.net/api'), $text);
 	}
 }
