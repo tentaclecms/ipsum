75195eaea591e2688192e4422f8ee3c8b4dd4927
Adam Patterson <hello@adampatterson.ca>
2013-01-01 17:36:54 -0700

    Plugin update, static call for ipsum content.

--- ipsum.php
+++ ipsum.php
@@ -6,7 +6,7 @@ class Ipsum extends Plugins {
 	}
 	
 	public function ipsum($text='') {
-		//return str_replace('[ipsum]', "plugin test", $text);
-		return str_replace('[ipsum]', file_get_contents('http://loripsum.net/api'), $text);
+		return str_replace('[ipsum]', "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ea mala virtuti magnitudine obruebantur. Ita nemo beato beatior. Quae quo sunt excelsiores, eo dant clariora indicia naturae. De vacuitate doloris eadem sententia erit. Atqui iste locus est, Piso, tibi etiam atque etiam confirmandus, inquam; In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Duo Reges: constructio interrete.", $text);
+		//return str_replace('[ipsum]', file_get_contents('http://loripsum.net/api'), $text);
 	}
 }
