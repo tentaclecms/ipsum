fac2bcfe8d4fbdffd2d167152d89cff9495625e8
Adam Patterson <hello@adampatterson.ca>
2013-01-13 00:20:08 -0700

    re-wrote for new plugin API

--- ipsum.php
+++ ipsum.php
@@ -1,13 +1,22 @@
 <?php
-class Ipsum extends Plugins {
+event::register('preview', 'ipsum::get', 1);
+event::register('plugin_navigation', 'ipsum::settings_nav', 8);
 
-	public function __init() {
-		$this->add_alias("preview", "ipsum", 1);
-        $this->add_alias("preview", "ipsum", 1);
-	}
-	
-	public function ipsum($text='') {
-        return str_replace('[ipsum]', "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ea mala virtuti magnitudine obruebantur. Ita nemo beato beatior. Quae quo sunt excelsiores, eo dant clariora indicia naturae. De vacuitate doloris eadem sententia erit. Atqui iste locus est, Piso, tibi etiam atque etiam confirmandus, inquam; In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Duo Reges: constructio interrete.", $text);
-		//return str_replace('[ipsum]', file_get_contents('http://loripsum.net/api'), $text);
-	}
+class ipsum
+{
+	static function settings_nav() {
+		$nav[] = array(
+            'title'     => 'Ipsum',
+            'rout'      => 'ipsum_settings',
+            'uri'       => 'ipsum/view'
+        );
+
+    	return $nav;
+    }
+
+	static function get( $text='' )
+    {
+        return str_replace('[ipsum]', "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ea mala virtuti magnitudine obruebantur. Ita nemo beato beatior. Quae quo sunt excelsiores, eo dant clariora indicia naturae. De vacuitate doloris eadem sententia erit. Atqui iste locus est, Piso, tibi etiam atque etiam confirmandus, inquam; In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Duo Reges: constructio interrete.</p>", $text);
+        //return str_replace('[ipsum]', file_get_contents('http://loripsum.net/api'), $text);
+    }
 }
