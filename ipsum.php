c76c618ebea6682926f63eccc25071e1ffd07aa7
Adam Patterson <hello@adampatterson.ca>
2013-01-18 23:44:38 -0700

    updated for new plugin api

--- ipsum.php
+++ ipsum.php
@@ -1,19 +1,8 @@
 <?php
-event::register('preview', 'ipsum::get', 1);
-event::register('plugin_navigation', 'ipsum::settings_nav', 8);
+event::on('preview', 'ipsum::get', 1);
 
 class ipsum
 {
-	static function settings_nav() {
-		$nav[] = array(
-            'title'     => 'Ipsum',
-            'rout'      => 'ipsum_settings',
-            'uri'       => 'ipsum/view'
-        );
-
-    	return $nav;
-    }
-
 	static function get( $text='' )
     {
         return str_replace('[ipsum]', "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ea mala virtuti magnitudine obruebantur. Ita nemo beato beatior. Quae quo sunt excelsiores, eo dant clariora indicia naturae. De vacuitate doloris eadem sententia erit. Atqui iste locus est, Piso, tibi etiam atque etiam confirmandus, inquam; In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Duo Reges: constructio interrete.</p>", $text);
