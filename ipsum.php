16de087e2f437debc861d95458324cd4a913a2fa
Adam Patterson <hello@adampatterson.ca>
2013-01-21 23:38:58 -0700

    Ipsum makes an API call now.

--- ipsum.php
+++ ipsum.php
@@ -14,7 +14,6 @@ class ipsum
 {
 	static function get( $text='' )
     {
-        return str_replace('[ipsum]', "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ea mala virtuti magnitudine obruebantur. Ita nemo beato beatior. Quae quo sunt excelsiores, eo dant clariora indicia naturae. De vacuitate doloris eadem sententia erit. Atqui iste locus est, Piso, tibi etiam atque etiam confirmandus, inquam; In quibus doctissimi illi veteres inesse quiddam caeleste et divinum putaverunt. Duo Reges: constructio interrete.</p>", $text);
-        //return str_replace('[ipsum]', file_get_contents('http://loripsum.net/api'), $text);
+       return str_replace('[ipsum]', file_get_contents('http://loripsum.net/api'), $text);
     }
 }
